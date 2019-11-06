cae609d428fa291b5ba968e3547a1a31feef9d9e
MaximGB <maximgb@yandex.ru>
2019-11-06 18:22:17 +0300

    Isolated subscriptions test added

---
+++ test/tetrisrf/xstate/subscriptions_test.cljs
@@ -0,0 +1,49 @@
+(ns tetrisrf.xstate.subscriptions-test
+  (:require [cljs.test :refer [deftest is testing async use-fixtures]]
+            [cljs.core.async :as casync]
+            [re-frame.core :as rf]
+            [tetrisrf.xstate.core :refer [machine
+                                          interpreter!
+                                          reg-isub
+                                          idb-action]]))
+
+(def rf-checkpoint (volatile! nil))
+
+(use-fixtures
+  :each
+  {:before (fn [] (vreset! rf-checkpoint (rf/make-restore-fn)))
+   :after (fn [] (@rf-checkpoint))})
+
+
+(deftest intrepreter-db-subscription-isolation-test
+  (testing "Subscribing to an isolated by interpreter app db part"
+    (async done
+           (let [c (casync/timeout 100)
+                 m (machine {:id :test-machine})
+                 i (interpreter! [:a :b :c] m)]
+
+             (rf/reg-event-db
+              ::update-db
+              (fn [db [_ path value]]
+                (assoc-in db path value)))
+
+             (rf/reg-event-db
+              ::next
+              (fn [db]
+                (casync/put! c ::next)
+                db))
+
+             (reg-isub
+              ::my-sub
+              identity)
+
+             (casync/go
+               ;; Resetting db
+               (rf/dispatch [::update-db nil {}])
+               ;; Updating db part under interpreter part manually
+               (rf/dispatch [::update-db [:a :b :c] ::my-value])
+               ;; Waiting for dispatch to be handled
+               (rf/dispatch [::next])
+               (casync/<! c)
+               (is (= @(rf/subscribe [::my-sub i]) ::my-value) "Subscription returned isolated app db part")
+               (done))))))
